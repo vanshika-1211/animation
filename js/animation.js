5e2fbe402d34c777603ccc06d2976c30f9eb83d3
vanshika-1211 <vanshika1910202@akgec.ac.in>
2020-08-24 23:54:32 +0530

    added bart and worked on js animation

--- js/animation.js
+++ js/animation.js
@@ -1,21 +1,17 @@
-const converted = {
-  "#multi-background": {
-    width: "100%",
-    height: "100vh",
-    backgroundColor: "#555",
-    backgroundImage:
-      "url('5.png'),\n                    url('4.png'),\n                    url('3.png'),\n                    url('2.png'),\n                    url('1.png')",
-    backgroundSize: "100vw 100%",
-    animation: "multi-background linear 100s infinite"
-  },
-  "@keyframes multi-background": {
-    from: {
-      backgroundPosition:
-        "0px 0px,\n                         0px 0px,\n                         0px 0px,\n                         0px 0px,\n                         0px 0px"
-    },
-    to: {
-      backgroundPosition:
-        "7800px 0px,\n                         6500px 0px,\n                         5200px 0px,\n                         3900px 0px,\n                         2600px 0px"
+let tID
+function animateWalk(){
+  const imgCount=8;
+  const totWidth=680px;
+  let position=totWidth/imgCount;
+  const interval=200;
+  const diff=totWidth/imgCount;
+  tID=setInterval(()=>{
+    document.getElementById('image').style.backgroundPosition='-${position}px 0px';
+    if(position<totWidth){
+      position=position+diff;
     }
-  }
-};
+    else {
+      position=totWidth/imgCount;
+    }
+  },interval)
+}
