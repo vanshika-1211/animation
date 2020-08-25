420d1de6e44cf4946b73f9a6460ff18fefea932e
vanshika-1211 <vanshika1910202@akgec.ac.in>
2020-08-26 00:47:26 +0530

    coded man sprite using js

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,22 @@
-startbtn.addEventListener("click", function(){
-  man.style.animation = "walk 2s steps(8) infinite";
-  man.style.animation = "forward 12s linear infinite";
+var tID;
+var sprite = document.getElementById('man');
+
+function animateScript() {
   bg.style.animation = "bg 50s linear infinite";
-});
-stopbtn.addEventListener("click", function(){
-  man.style.animation = "";
-  man.style.animation = "";
+  var position = 0;
+  const interval = 150;
+  const diff = 148;
+  tID = setInterval(() => {
+    sprite.style.backgroundPosition = `-${position}px 0px`;
+    if (position < 1186) {
+      position = position + diff;
+    } else {
+      position = 0;
+    }
+  }, interval);
+}
+
+function stopAnimate() {
   bg.style.animation = "";
-});
+  clearInterval(tID);
+}
