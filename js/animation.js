441779d801cf5a21836688c6192466d7f48f8c60
vanshika-1211 <vanshika1910202@akgec.ac.in>
2020-08-25 17:47:24 +0530

    trying to add start stop animation using js

--- js/animation.js
+++ js/animation.js
@@ -1,17 +1,9 @@
-let tID
-function animateWalk(){
-  const imgCount=8;
-  const totWidth=680px;
-  let position=totWidth/imgCount;
-  const interval=200;
-  const diff=totWidth/imgCount;
-  tID=setInterval(()=>{
-    document.getElementById('image').style.backgroundPosition='-${position}px 0px';
-    if(position<totWidth){
-      position=position+diff;
-    }
-    else {
-      position=totWidth/imgCount;
-    }
-  },interval)
-}
+man.addEventListener("click", function start() {
+  man.style.animation="walk 1s steps(8) infinite";
+  man.style.animation="forward 12s linear infinite";
+  multi-background="multi-background 50s linear infinite";
+});
+man.addEventListener("click", function stop() {
+  man.style.animation="";
+  multi-background="";
+});
